b0a0de9944907636a101956d16f8c06186b41d62
bharat-basalanalytics <bharat@basalanalytics.com>
2026-05-14 13:17:35 +0530

    wip

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 const CONSTANTS = {
-  API: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8800/api/v1',
+  API: process.env.NEXT_PUBLIC_API_URL || 'https://calling-demo-backend.onrender.com/api/v1',
   TOKEN_KEY: process.env.NEXT_PUBLIC_TOKEN_KEY || 'user_session_token'
 }
 
