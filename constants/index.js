43df52e75be66aa8e32394dc95989abd8c55f074
bharat-basalanalytics <bharat@basalanalytics.com>
2026-05-14 14:33:12 +0530

    wip

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,5 @@
 const CONSTANTS = {
-  API: process.env.NEXT_PUBLIC_API_URL || 'https://calling-demo-backend.onrender.com/api/v1',
-  TOKEN_KEY: process.env.NEXT_PUBLIC_TOKEN_KEY || 'user_session_token'
+  API: 'https://calling-demo-backend.onrender.com/api/v1'
 }
 
 export default CONSTANTS
